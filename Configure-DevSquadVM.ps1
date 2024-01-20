4a35da02dbddc58c52eb593fa4c8e82488cd7d73
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-20 12:38:26 -0800

    adding write-host output

--- Configure-DevSquadVM.ps1
+++ Configure-DevSquadVM.ps1
@@ -1,11 +1,16 @@
 param([String]$azureFilesKey)
 
+Write-Host "Applying Timezone Redirecting settings"
 ./Set-TimezoneRedirection.ps1
 
+Write-Host "Applying Windows Optimizations"
 ./Set-WindowsOptimization -Optimizations 'WindowsMediaPlayer','ScheduledTasks','DefaultUserSettings','Autologgers','Services','NetworkOptimizations','LGPO','DiskCleanup','Edge','RemoveLegacyIE'
 
+Write-Host "Creating DevDrive"
 ./Create-DevDrive.ps1
 
+Write-Host "Mounting Azure Files 'software' share"
 ./Mount-AzureFiles.ps1 -account squadstorage -share software -key $azureFilesKey
 
+Write-Host "Installing tools"
 ./Install-Tools.ps1
