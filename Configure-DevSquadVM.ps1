68ad98a80a7dab3ab13755f48856fdbfd5ea3b16
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-21 17:06:33 -0800

    remove windows optimizations scriptrun

--- Configure-DevSquadVM.ps1
+++ Configure-DevSquadVM.ps1
@@ -3,8 +3,8 @@ param([String]$azureFilesKey)
 Write-Host "Applying Timezone Redirecting settings"
 & $PSScriptRoot\Set-TimezoneRedirection.ps1
 
-Write-Host "Applying Windows Optimizations"
-& $PSScriptRoot\Set-WindowsOptimization -Optimizations 'WindowsMediaPlayer','ScheduledTasks','DefaultUserSettings','Autologgers','Services','NetworkOptimizations','LGPO','DiskCleanup','Edge','RemoveLegacyIE'
+# Write-Host "Applying Windows Optimizations"
+# & $PSScriptRoot\Set-WindowsOptimization -Optimizations 'WindowsMediaPlayer','ScheduledTasks','DefaultUserSettings','Autologgers','Services','NetworkOptimizations','LGPO','DiskCleanup','Edge','RemoveLegacyIE'
 
 Write-Host "Creating DevDrive"
 & $PSScriptRoot\Create-DevDrive.ps1
