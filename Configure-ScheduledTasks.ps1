e09fcc4cd40cce8616bbe12c7bf2c82d9732c9f7
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-24 07:59:17 -0800

    fix scheduled tasks script

--- Configure-ScheduledTasks.ps1
+++ Configure-ScheduledTasks.ps1
@@ -4,12 +4,12 @@ Write-Host "Adding DevDrive Mount scheduled task"
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "Mount-Vhd c:\devdrive.vhdx"
 $Trigger = New-ScheduledTaskTrigger -AtLogon
 $Principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
-$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -Hidden -MultipleInstances -IgnoreNew 
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -Hidden -MultipleInstances IgnoreNew 
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings
 Register-ScheduledTask -TaskName "Mount DevDrive" -InputObject $Task
 
 Write-Host "Adding S: mount scheduled task"
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "c:\scripts\Mount-AzureFiles.ps1 -key $azureFilesKey"
-$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -Hidden -MultipleInstances -IgnoreNew -RunOnlyIfNetworkAvailable
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -Hidden -MultipleInstances IgnoreNew -RunOnlyIfNetworkAvailable
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
 Register-ScheduledTask -TaskName "Mount Squad Software storage" -InputObject $Task
