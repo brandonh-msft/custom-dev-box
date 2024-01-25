cde40cff249a1746a6f60c567d3c8b4aa6624926
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-25 14:48:41 -0800

    MTA mode for powershell calls

--- Update-WinGetPackages.ps1
+++ Update-WinGetPackages.ps1
@@ -1,5 +1,5 @@
 Install-Module Microsoft.WinGet.Client -AcceptLicense -Force
 
-$pwshOutput = (pwsh -c "Get-WingetPackage | Where-Object IsUpdateAvailable -eq $true | Where-Object Id -ne 'Microsoft.Office' | ConvertTo-Json")
+$pwshOutput = (pwsh -MTA -c "Get-WingetPackage | Where-Object IsUpdateAvailable -eq $true | Where-Object Id -ne 'Microsoft.Office' | ConvertTo-Json")
 $appsToUpdate = ($pwshOutput | ConvertFrom-Json) | Where-Object IsUpdateAvailable -eq $true | Select-Object -ExpandProperty Id
-pwsh -cwa '$args | % { Write-Output \"Updating $_ ...\" ; Update-WingetPackage -Id $_ -Mode Silent -Force -MatchOption EqualsCaseInsensitive }' $appsToUpdate
+pwsh -MTA -cwa '$args | % { Write-Output \"Updating $_ ...\" ; Update-WingetPackage -Id $_ -Mode Silent -Force -MatchOption EqualsCaseInsensitive }' $appsToUpdate
