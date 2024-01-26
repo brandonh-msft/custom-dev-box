cb60b119849c9ecfc9b639b48e56d8841ac1d9c6
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-25 16:41:56 -0800

    Always exit 0 for winget updates

--- Update-WinGetPackages.ps1
+++ Update-WinGetPackages.ps1
@@ -3,3 +3,5 @@ Install-Module Microsoft.WinGet.Client -AcceptLicense -Force
 $pwshOutput = (pwsh -MTA -c "Get-WingetPackage | Where-Object IsUpdateAvailable -eq $true | Where-Object Id -ne 'Microsoft.Office' | ConvertTo-Json")
 $appsToUpdate = ($pwshOutput | ConvertFrom-Json) | Where-Object IsUpdateAvailable -eq $true | Select-Object -ExpandProperty Id
 pwsh -MTA -cwa '$args | % { Write-Output \"Updating $_ ...\" ; Update-WingetPackage -Id $_ -Mode Silent -Force -MatchOption EqualsCaseInsensitive }' $appsToUpdate
+
+exit 0
