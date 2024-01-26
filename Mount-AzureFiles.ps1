79214fb7afdda8aae711013e7736c79e72f5fc8a
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-26 12:05:54 -0800

    Updating azure files script, mount share globally

--- Mount-AzureFiles.ps1
+++ Mount-AzureFiles.ps1
@@ -1,11 +1,11 @@
-param([string]$key, [string]$account="squadstorage", [string]$share="software")
+param([string]$key, [string]$account="squadstorage", [string]$share="software", [string]$driveletter="S")
 
 $connectTestResult = Test-NetConnection -ComputerName "${account}.file.core.windows.net" -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
     cmd.exe /C "cmdkey /add:`"${account}.file.core.windows.net`" /user:`"Azure\${account}`" /pass:`"${key}`""
     # Mount the drive
-    New-PSDrive -Name S -PSProvider FileSystem -Root "\\${account}.file.core.windows.net\${share}" -Persist
+    New-PSDrive -Name $driveletter -PSProvider FileSystem -Root "\\${account}.file.core.windows.net\${share}" -Persist -Scope Global
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
