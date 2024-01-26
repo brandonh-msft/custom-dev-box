ecf89969ac40aba5875694d6cd86dccd71358395
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-26 14:24:00 -0800

    installing via winget where possible

--- Install-Tools.ps1
+++ Install-Tools.ps1
@@ -3,12 +3,8 @@ $ErrorActionPreference = "Stop"
 Write-Host "Installing software via Chocolatey..."
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Installing Notepad++"
-choco install notepadplusplus -y -f -r --no-progress --ignoredetectedreboot 
 Write-Host "Installing VS Code"
 choco install vscode -y -f -r --no-progress --ignoredetectedreboot 
-Write-Host "Installing GPG4Win"
-choco install gpg4win -y -f -r --no-progress --ignoredetectedreboot 
 Write-Host "Installing 7Zip"
 choco install 7zip -y -f -r --no-progress --ignoredetectedreboot 
 Write-Host "Installing Powershell Core"
@@ -17,21 +13,33 @@ Write-Host "Installing Git Credential Manager"
 choco install git-credential-manager-for-windows -y -f -r --no-progress --ignoredetectedreboot
 Write-Host "Installing Docker Desktop"
 choco install docker-desktop -y -f -r --no-progress --ignoredetectedreboot 
-Write-Host "Installing Windows Powertoys"
-choco install powertoys -y -f -r --no-progress --ignoredetectedreboot 
-Write-Host "Installing Microsoft Azure Storage Explorer"
-choco install microsoftazurestorageexplorer -y -f -r --no-progress --ignoredetectedreboot 
-Write-Host "Installing Python 3"
-choco install python3 -y -f -r --no-progress --ignoredetectedreboot
 Write-Host "Installing Go"
 choco install golang -y -f -r --no-progress --ignoredetectedreboot
 Write-Host "Installing Terraform"
 choco install terraform -y -f -r --no-progress --ignoredetectedreboot
 Write-Host "Installing Bicep"
 choco install bicep -y -f -r --no-progress --ignoredetectedreboot
-# Write-Host "Installing Office"
-# choco install office365business -y
-# Write-Host "Installing Teams"
-# choco install Microsoft-teams -y
 
+Write-Host "Installing software via WinGet..."
 Install-Module Microsoft.WinGet.Client -AcceptLicense -Force
+
+Write-Host "Installing Notepad++"
+pwsh -MTA -c "Install-WingetPackage -Id 'NotepadPlusPlus_7njy0v32s6xk6' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# choco install notepadplusplus -y -f -r --no-progress --ignoredetectedreboot 
+Write-Host "Installing GPG4Win"
+pwsh -MTA -c "Install-WingetPackage -Id 'GnuPG.Gpg4win' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# choco install gpg4win -y -f -r --no-progress --ignoredetectedreboot 
+Write-Host "Installing Dev Home"
+pwsh -MTA -c "Install-WingetPackage -Id 'Microsoft.DevHome' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+pwsh -MTA -c "Install-WingetPackage -Id 'Microsoft.Windows.DevHomeAzureExtension_8wekyb3d8bbwe' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+pwsh -MTA -c "Install-WingetPackage -Id 'Microsoft.Windows.DevHomeGitHubExtension_8wekyb3d8bbwe' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+Write-Host "Installing Windows Powertoys"
+pwsh -MTA -c "Install-WingetPackage -Id 'Microsoft.PowerToys' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# choco install powertoys -y -f -r --no-progress --ignoredetectedreboot 
+Write-Host "Installing Microsoft Azure Storage Explorer"
+pwsh -MTA -c "Install-WingetPackage -Id 'Microsoft.Azure.StorageExplorer' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# choco install microsoftazurestorageexplorer -y -f -r --no-progress --ignoredetectedreboot 
+Write-Host "Installing Python 3"
+pwsh -MTA -c "Install-WingetPackage -Id 'Python.Launcher' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# pwsh -MTA -c "Install-WingetPackage -Id 'Python.Python.3.11' -Mode Silent -Force -MatchOption EqualsCaseInsensitive -Scope System"
+# choco install python3 -y -f -r --no-progress --ignoredetectedreboot
