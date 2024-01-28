1f8bbd047c52924ee44b057883a8e60c9f73b809
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-28 14:13:15 -0800

    Checking for winget before trying to install

--- Install-Tools.ps1
+++ Install-Tools.ps1
@@ -1,19 +1,27 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Installing winget CLI..."
-# Get the download URL of the latest winget installer from GitHub:
-$API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url |
-    Where-Object {$_.EndsWith(".msixbundle")}
+try {
+    Write-Host "Checking for Winget..."
 
-# Download the installer:
-Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
+    winget list
+}
+catch {
+    Write-Host "Winget is not installed. Installing..."
 
-# Install winget:
-Add-AppxPackage winget.msixbundle
+    # Get the download URL of the latest winget installer from GitHub:
+    $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url |
+    Where-Object { $_.EndsWith(".msixbundle") }
 
-# Remove the installer:
-Remove-Item winget.msixbundle
+    # Download the installer:
+    Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
+
+    # Install winget:
+    Add-AppxPackage winget.msixbundle
+
+    # Remove the installer:
+    Remove-Item winget.msixbundle
+}
 
 Write-Host "Installing software via winget..."
 
