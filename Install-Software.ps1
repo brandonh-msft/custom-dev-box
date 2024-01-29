92668a0f8edfda4ded973cbb144ffd5187c4801e
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-01-29 10:21:11 -0800

    adding install status output to winget installations

--- Install-Software.ps1
+++ Install-Software.ps1
@@ -4,26 +4,36 @@ Install-Module Microsoft.WinGet.Client -Scope AllUsers -AcceptLicense -AllowClob
 Import-Module Microsoft.WinGet.Client -Force -Global
 
 Write-Host "Installing VS Code"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.VisualStudioCode
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.VisualStudioCode | Write-Status
 Write-Host "Installing 7Zip"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id 7zip.7zip
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id 7zip.7zip | Write-Status
 Write-Host "Installing Docker Desktop"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Docker.DockerDesktop 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Docker.DockerDesktop | Write-Status
 Write-Host "Installing Go"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id GoLang.Go 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id GoLang.Go | Write-Status
 Write-Host "Installing Bicep"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.Bicep 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.Bicep | Write-Status
 Write-Host "Installing Terraform"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Hashicorp.Terraform 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Hashicorp.Terraform | Write-Status
 Write-Host "Installing Notepad++"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id 'Notepad++.Notepad++' 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id 'Notepad++.Notepad++' | Write-Status
 Write-Host "Installing GPG4Win"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id GnuPG.Gpg4win 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id GnuPG.Gpg4win | Write-Status
 Write-Host "Installing Dev Home"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.DevHome 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.DevHome | Write-Status
 Write-Host "Installing Windows Powertoys"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.PowerToys 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.PowerToys | Write-Status
 Write-Host "Installing Microsoft Azure Storage Explorer"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.Azure.StorageExplorer 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Microsoft.Azure.StorageExplorer | Write-Status
 Write-Host "Installing Python 3"
-Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Python.Launcher 
+Install-WinGetPackage -Scope SystemOrUnknown -Mode Silent -MatchOption EqualsCaseInsensitive -Force -Id Python.Launcher | Write-Status
+
+function Write-Status {
+    process {
+        if ($_.Status -eq 'Ok') {
+            Write-Output "✅"
+        } else {
+            Write-Output "$($_.Status) $($_.ExtendedErrorCode)"
+        }
+    }
+}
