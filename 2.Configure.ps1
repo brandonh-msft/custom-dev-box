eccf1a3774158676338dc8bd4ca48bea45e32250
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-02-02 09:54:12 -0800

    don't require network connectivity for devdrive mounting

--- 2.Configure.ps1
+++ 2.Configure.ps1
@@ -23,6 +23,7 @@ Start-WithStatus "Adding S: mount scheduled task" {
 Start-WithStatus "Adding DevDrive mount scheduled task" { 
     $Action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "Mount-VHD -Path c:\devdrive.vhdx"
     $Principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users" -RunLevel Highest
+    $Settings = New-ScheduledTaskSettingsSet -Compatibility Win8 -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -MultipleInstances IgnoreNew
     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings
     Register-ScheduledTask -TaskName "Mount Dev Drive" -InputObject $Task
 }
