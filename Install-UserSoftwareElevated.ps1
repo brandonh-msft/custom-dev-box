96ee66a915665b0fc80fc49d1d253735f2fd6975
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-02-05 14:00:00 -0800

    don't need to install bicep
    it's baked into the azure cli which is already on the box.

--- Install-UserSoftwareElevated.ps1
+++ Install-UserSoftwareElevated.ps1
@@ -3,7 +3,6 @@
 Install-PackageWithStatus -packageId Microsoft.DevHome -packageName "Dev Home"
 Install-PackageWithStatus -packageId 9MV8F79FGXTR -packageName "Dev Home Azure Extension"
 Install-PackageWithStatus -packageId 9NZCC27PR6N6 -packageName "Dev Home GitHub Extension"
-Install-PackageWithStatus -packageId Microsoft.Bicep -packageName "Bicep"
 Install-PackageWithStatus -packageId Hashicorp.Terraform -packageName "Terraform"
 
 Start-WithStatus "Updating WinGet packages" { $(Get-WinGetPackage | Where-Object { $_.IsUpdateAvailable -eq $true -and $_.Name -ne 'Microsoft 365 Apps for enterprise' }) | Update-WinGetPackage -Mode Silent -Force }
